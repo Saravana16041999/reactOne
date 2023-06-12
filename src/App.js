dfc174a7198d5e31f08b9c78cc7c6ede78005d63
Saravana16041999 <saravanakumar16041999@gmail.com>
2023-06-12 18:33:32 +0530

    corrections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import "./App.css";
 import { FaUserCircle } from "react-icons/fa";
 import { v4 as uuid } from "uuid";
+import validator from "validator";
 
 function App() {
   const [user, setUser] = useState(
@@ -9,14 +10,18 @@ function App() {
       ? JSON.parse(localStorage.getItem("users"))
       : []
   );
-  const [error, setError] = useState(false);
+  const [error, setError] = useState();
   const email = useRef("");
   const password = useRef("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (email.current.value === "" || password.current.value === "") {
-      setError(true);
+      setError("Enter a Valaid Email and Password");
+    } else if (password.current.value.length < 7) {
+      setError("Password must be more then 7 characters");
+    } else if (!validator.isEmail(email.current.value)) {
+      setError("Enter Valid Email @example.com");
     } else {
       const UserDetials = {
         id: uuid(),
@@ -25,19 +30,17 @@ function App() {
       };
       setUser([...user, UserDetials]);
       localStorage.setItem("users", JSON.stringify(user));
+      setError(false);
     }
   };
-
   return (
     <div className="App">
       <div className="main-container">
         <FaUserCircle className="user-logo" />
-        {error && (
-          <small className="worning">Enter your email and Password</small>
-        )}
+        {error && <small className="worning">{error}</small>}
         <form onSubmit={handleSubmit}>
-          <input type="text" ref={email} placeholder="enter email" />
-          <input type="password" placeholder="enter passsword" ref={password} />
+          <input type="text" ref={email} placeholder="Email" />
+          <input type="password" placeholder="Password" ref={password} />
           <button>Login</button>
         </form>
         <p>Forgot password?</p>
